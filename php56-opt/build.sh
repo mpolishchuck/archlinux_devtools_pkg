08dac19f5769f06667c93a5f532f96e20967a461
Paul N. Maxwell <msg.maxwel@gmail.com>
2018-12-02 12:03:34 +0200

    Test build for PHP 5.6 /opt

--- php56-opt/build.sh
+++ php56-opt/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-for DIR in php*; do
+cd php56-opt
+makepkg && makepkg --source || exit 1
+sudo pacman -U --noconfirm *.pkg.tar.xz
+cd ..
+
+for DIR in php56-opt-*; do
     cd $DIR
-    makepkg && makepkg --source || break
+    makepkg && makepkg --source || exit 1
     cd ..
 done
